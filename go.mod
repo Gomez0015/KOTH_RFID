3e7629a2bbe793d2aea33d4ec8b20137d394bc02
Sharqo78 <salime3ccollege@gmail.com>
2022-10-28 13:02:44 +0200

    test

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
-module main
+module Gomez0015/KOTH_RFID
 
-go 1.18
+go 1.19
 
 require (
 	github.com/labstack/echo/v4 v4.9.1 // indirect
--- main.go
+++ main.go
@@ -3,26 +3,13 @@ package main
 import (
 	"net/http"
 
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	// Echo instance
 	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// Routes
-	e.GET("/", hello)
-
-	// Start server
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
 	e.Logger.Fatal(e.Start(":1323"))
-}
-
-// Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
 }
